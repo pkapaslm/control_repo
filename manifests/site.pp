6e3c80a365a2c53b795aa4262f8eada2c725ce76
pkapaslm <98427606+pkapaslm@users.noreply.github.com>
2022-01-26 13:17:33 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
 node 'ip-10-55-45-108.us-west-2.compute.internal' {
-  # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
-  
-  # Configure the Puppet master to use puppetdb
-  class { 'puppetdb::master::config': }
 }
 
 node 'ip-10-55-45-237.us-west-2.compute.internal' {
